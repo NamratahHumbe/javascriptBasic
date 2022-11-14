f1515dae38ef9b52a83f8d12f59e3f0de8ca5cec
Namrata Humbe <namratah.humbe@thinkitive.com>
2022-11-14 17:28:25 +0530

    Today's update

--- class.js
+++ class.js
@@ -1,3 +1,24 @@
+//let
+let myname='Namrata';
+console.log(myname);
+myname='Pallavi'
+console.log(myname);
+
+//const
+const name='Namrata';
+console.log(myname);
+//name='Pallavi'  //gives error
+//console.log(myname);
+
+//Arrow function
+/*const square=(number)=>{
+    return number**2;*/
+const square=(number)=> number**2;
+//}
+console.log(square(4));
+
+//
+//Class
 function makeEmployee(){
     class Employee{
         constructor(id,name,location){
@@ -11,3 +32,29 @@ function makeEmployee(){
 const Employee=makeEmployee();
 const Namrata=new Employee(353,'Namrata','Pune');
 console.log(Namrata.location);
+
+//inheritance
+class Human{
+    constructor(){
+        this.gender='male'
+    }
+    printGender(){
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human{
+    constructor(){
+        super(); //to inherit properties
+        this.name="Namrata";
+        //this.gender="Female";
+    }
+    printMyName(){
+        console.log(this.name)
+    }
+}
+
+const person=new Person();
+person.printMyName();
+person.printGender();
+
